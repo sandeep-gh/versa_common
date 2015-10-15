58c7c7f0bc733b4d24fa542079c9e41ac4c28860
Sandeep Gupta <sandeep@vbi.vt.edu>
2015-10-15 14:47:38 -0400

    metadata and xmlutils files

---
+++ metadata_utils.py
@@ -0,0 +1,46 @@
+import xmlutils as xu
+
+#element,
+def read_metadata(metadata_fn):
+    return xu.read_file(metadata_fn)
+    
+def get_model_name(metadata_root):
+    #<model>modelname</model>
+    model_name=xu.get_value_by_attr(metadata_root, 'model')
+    return model_name
+
+
+def get_columns_and_type(metadata_root):
+    #<columns><column></column>
+    #         <column></column>
+    #</columns>
+    all_columns=[]
+    for col_elem in xu.get_elems_by_parent_child(metadata_root, 'columns', 'column'):
+        col_name=xu.get_value_by_attr(col_elem,'name')
+        col_type=xu.get_value_by_attr(col_elem,'type')
+        all_columns.append([col_name, col_type])
+    return all_columns
+
+def is_fkey(metadata_root, column_name):
+    xroot=xu.get_elems_by_parent_child_key_value(metadata_root, 'columns', 'column', 'name', column_name, True)
+    if 'fkey' in xu.get_attr_list(xroot):
+        return True
+    return False
+    
+
+def get_primary_keys(metadata_root):
+    all_primary_keys=[]
+    key_xroots=xu.get_elems_by_parent_child(metadata_root, 'primarykey', 'key')
+    for key_xroot in key_xroots:
+        pk_text=xu.get_value(key_xroot)
+        all_primary_keys.append(pk_text)
+    return all_primary_keys
+
+def get_foreign_keys(metadata_root):
+    all_foreign_keys=[]
+    for col_name, col_type in get_columns_and_type(metadata_root):
+        if is_fkey(metadata_root, col_name):
+            all_foreign_keys.append(col_name)
+
+    return all_foreign_keys
+ 
---
+++ xmlutils.py
@@ -0,0 +1,136 @@
+import xml.etree.ElementTree as ET
+
+#element is xml tree node
+#keys are elements that are queried
+#value is string literal
+
+def read_file(xml_fn):
+    xmldoc = ET.parse(xml_fn)
+    return xmldoc
+
+def get_attr_list(root):
+    #<root attr1='', attr2=''></root>
+    return root.keys()
+
+
+def get_value(root):
+    return root.text.strip()
+
+def get_elems(root, attrname, uniq=False):
+    all_attr_elems=root.findall(".//"+attrname)
+    if uniq:
+        assert(len(all_attr_elems) == 1)
+        return all_attr_elems[0]
+    return all_attr_elems
+
+def get_value_elems(root, attrname):
+    values=[]
+    for elem in get_elems(root, attrname):
+        values.append(get_value(elem))
+    return values
+                      
+
+#<attr>value<attr9>
+def get_value_by_attr(root, attrname):
+    elem=get_elems(root, attrname, True)
+    return elem.text.strip()
+    
+
+
+#<elemn><key>value</key></elemn>
+def get_elem_by_key_value(xmldoc, elemn, key,value, uniq=False):
+    all_elems = xmldoc.findall('.//'+elemn+'[' + key+ '=\'' +value+'\']') 
+    if uniq:
+        assert(len(all_elems) == 1)
+        return all_elems[0]
+    return all_elems
+
+def get_uniq_elem_by_key_value(xmldoc, elemn, key,value):
+    return get_elem_by_key_value(xmldoc, elemn, key,value, uniq=True)
+
+def get_elems_by_parent_child(root, parent, child):
+    #get all roots that are of type <parent><child></child></parent>
+    return root.findall(".//"+parent+"/"+child)
+
+
+#Give that element whose parent=parent and has one child element as <key>value</key>
+def get_elems_by_parent_child_key_value(root,parent, child, key, value, uniq=False):
+    #<parent>
+    #  <child>
+    #    <key>value</key>
+    #  </child>
+    #</parent>
+    xpath_str=".//"+parent+"/"+child+"["+key+"=\'"+value+"\']"
+    childnodes=root.findall(xpath_str)
+    if uniq:
+        if(len(childnodes) == 0):
+            print xpath_str
+            assert(0)
+        assert(len(childnodes) == 1)
+        return childnodes[0]
+    return childnodes
+
+
+
+
+class XmlListConfig(list):
+    def __init__(self, aList):
+        for element in aList:
+            if element:
+                # treat like dict
+                if len(element) == 1 or element[0].tag != element[1].tag:
+                    self.append(XmlDictConfig(element))
+                # treat like list
+                elif element[0].tag == element[1].tag:
+                    self.append(XmlListConfig(element))
+            elif element.text:
+                text = element.text.strip()
+                if text:
+                    self.append(text)
+
+
+class XmlDictConfig(dict):
+    '''
+    Example usage:
+
+    >>> tree = ElementTree.parse('your_file.xml')
+    >>> root = tree.getroot()
+    >>> xmldict = XmlDictConfig(root)
+
+    Or, if you want to use an XML string:
+
+    >>> root = ElementTree.XML(xml_string)
+    >>> xmldict = XmlDictConfig(root)
+
+    And then use xmldict for what it is... a dict.
+    '''
+    def __init__(self, parent_element):
+        if parent_element.items():
+            self.update(dict(parent_element.items()))
+        for element in parent_element:
+            if element:
+                # treat like dict - we assume that if the first two tags
+                # in a series are different, then they are all different.
+                if len(element) == 1 or element[0].tag != element[1].tag:
+                    aDict = XmlDictConfig(element)
+                # treat like list - we assume that if the first two tags
+                # in a series are the same, then the rest are the same.
+                else:
+                    # here, we put the list in dictionary; the key is the
+                    # tag name the list elements all share in common, and
+                    # the value is the list itself 
+                    aDict = {element[0].tag: XmlListConfig(element)}
+                # if the tag has attributes, add those to the dict
+                if element.items():
+                    aDict.update(dict(element.items()))
+                self.update({element.tag: aDict})
+            # this assumes that if you've got an attribute in a tag,
+            # you won't be having any text. This may or may not be a 
+            # good idea -- time will tell. It works for the way we are
+            # currently doing XML configuration files...
+            elif element.items():
+                self.update({element.tag: dict(element.items())})
+            # finally, if there are no child tags and no attributes, extract
+            # the text
+            else:
+                self.update({element.tag: element.text})
